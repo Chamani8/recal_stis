2ae4c48628fb33058e36cc742b77f75a94d3ced0
Chamani Gunasekera <cgunasekera@chamani.local>
2025-02-02 22:55:11 -0500

    Add file options for x1d

--- recalibrate_stis_data.py
+++ recalibrate_stis_data.py
@@ -20,23 +20,23 @@ def delete_files(del_filetypes, file_loc):
         command_args.extend(file for file in del_files)
     subprocess.run(command_args)
 
-def run_x1d(sci_infile, x1d_save_path, prod_type = "drj", out_prod_type = "sx1"):
-    infiles = glob.glob(f"{sci_infile}*{prod_type}.fits")
+def run_x1d(sci_infile, x1d_save_path, x1d_infile_type, x1d_outfile_type):
+    infiles = glob.glob(f"{sci_infile}*{x1d_infile_type}.fits")
 
     for file in infiles:
         infile = file.split("/")[-1]
-        x1d_outfile = infile.replace(f"{prod_type}", f"{prod_type}_{out_prod_type}")
+        x1d_outfile = infile.replace(f"{x1d_infile_type}", f"{x1d_infile_type}_{x1d_outfile_type}")
         rootname = file.split("/")[-1].split("_")[0]
         
-    delete_files([f"{prod_type}_x1d.fits"], file_loc=sci_infile)
+    delete_files([f"{x1d_infile_type}_x1d.fits"], file_loc=sci_infile)
     stistools.x1d.x1d(infile, output=x1d_outfile, verbose=True, trailer=f"{rootname}.trl") # .trl acts like nohup
     print(f"File written:    {x1d_outfile}")
 
     # Copy new x1d/sx1 files to datapath for extinction measure
     if x1d_save_path != None:
-        command_args = ["cp", f"{x1d_outfile}", f"{x1d_save_path}{rootname}_{out_prod_type}.fits"]
+        command_args = ["cp", f"{x1d_outfile}", f"{x1d_save_path}{rootname}_{x1d_outfile_type}.fits"]
         subprocess.run(command_args)
-        print(f"1D spectra fits file copied to:   {x1d_save_path}{rootname}_{out_prod_type}.fits")
+        print(f"1D spectra fits file copied to:   {x1d_save_path}{rootname}_{x1d_outfile_type}.fits")
 
 
 def defringe(sci_infile, flat_infile):
@@ -56,7 +56,7 @@ def defringe(sci_infile, flat_infile):
     # 1. Normalize the Fringe Flat
     stistools.defringe.normspflat(f"{flat_infile}_raw.fits",
                               f"{flat_infile}_nsp.fits", do_cal=True,
-                              wavecal=f"{sci_infile}_raw.fits") 
+                              wavecal=f"{sci_infile}_raw.fits")
 
     # Flatten the blue end of the flat-field image [ONLY FOR G750L]
     with fits.open(f"{flat_infile}_nsp.fits", mode='update') as hdulist:
@@ -90,12 +90,12 @@ def defringe(sci_infile, flat_infile):
 
     stistools.defringe.defringe(f"{sci_outfile}_{prod_type}.fits", f"{flat_outfile}_frr.fits", overwrite=True)
 
-def main(sci_infile, flat_infile, x1d_save_path):
+def main(sci_infile, flat_infile, x1d_infile_type, x1d_outfile_type, x1d_save_path):
     if flat_infile != None: flat_infile =  fits.getheader(f"{sci_infile}_raw.fits",0)['FRNGFLAT'].lower()
 
     try:
         defringe(sci_infile, flat_infile)
-        run_x1d(sci_infile, x1d_save_path)
+        run_x1d(sci_infile, x1d_save_path, x1d_infile_type, x1d_outfile_type)
     except:
         print("Whoops! Need to add path variable for reference files. Run the following in the terminal, and retry.\n")
         print(" conda activate stenv\n export CRDS_PATH=\"$HOME/crds_cache\" \n export CRDS_SERVER_URL=\"https://hst-crds.stsci.edu\" \n export oref=\"${CRDS_PATH}/hst/oref/\"")
@@ -106,7 +106,9 @@ if __name__ == "__main__":
     parser.add_argument('sci_infile', type=str, help="Science input filename.")
     parser.add_argument('--flat_infile', type=str, default=None, help="Flat input filename.")
     parser.add_argument('--x1d_save_path', type=str, default=None, help="Path other than current working directory, to save x1d file.")
+    parser.add_argument('--x1d_infile_type', type=str, default="drj", help="Path other than current working directory, to save x1d file.")
+    parser.add_argument('--x1d_outfile_type', type=str, default="sx1", help="Path other than current working directory, to save x1d file.")
 
     args = parser.parse_args()
 
-    main(args.sci_infile, args.flat_infile, args.x1d_save_path)
+    main(args.sci_infile, args.flat_infile, args.x1d_infile_type, args.x1d_outfile_type, args.x1d_save_path)
